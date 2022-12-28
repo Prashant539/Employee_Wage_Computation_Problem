7562d030a967beb788280fdd941947c75afc9a6b
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-28 22:01:03 +0530

    [Prashant]Employee Wage: Wages till number of working days

--- EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
@@ -13,12 +13,15 @@ namespace EmployeeWageProblem
         public const int IS_FUlL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int Num_Of_working_Days = 3;
+        public const int Max_Hrs_In_Month = 10;
         public void CheckEmployee()
         {
             int empHrs = 0;
             int empWages = 0;
             int totalempWage = 0;
-            for (int day = 0; day < Num_Of_working_Days; day++)
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHrs <= Max_Hrs_In_Month && totalWorkingDays < Num_Of_working_Days)
             {
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
@@ -38,11 +41,11 @@ namespace EmployeeWageProblem
                         Console.WriteLine("Employee is absent");
                         break;
                 }
-                empWages = empHrs * EMP_RATE_PER_HOUR;
-                totalempWage += empWages;
-                Console.WriteLine("Emp Wage : " + empWages);
+                totalEmpHrs = totalEmpHrs + empHrs;
+                Console.WriteLine("Day : " + totalWorkingDays + "Emp Hrs: " + empHrs);
             }
-            Console.WriteLine("Total EmpWage:" + totalempWage);
+            totalempWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage for a Month: " + totalempWage);
         }
     }
 }
