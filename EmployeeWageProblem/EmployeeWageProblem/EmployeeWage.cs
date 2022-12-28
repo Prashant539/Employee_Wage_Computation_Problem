80ecfa21e9802fbcb5f32ddaecd320f7d5f2d17f
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-28 21:49:42 +0530

    [Prashant]Employee Wage: Wages for a month

--- EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
@@ -9,33 +9,40 @@ namespace EmployeeWageProblem
     public class EmployeeWage
     {
 
-       public const int IS_PART_TIME = 1;
-       public const int IS_FUlL_TIME = 2;
-       public const int EMP_RATE_PER_HOUR = 20;
+        public const int IS_PART_TIME = 1;
+        public const int IS_FUlL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int Num_Of_working_Days = 3;
         public void CheckEmployee()
         {
             int empHrs = 0;
             int empWages = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int totalempWage = 0;
+            for (int day = 0; day < Num_Of_working_Days; day++)
             {
-                case IS_FUlL_TIME:
-                    Console.WriteLine("Employee is full time");
-                    empHrs = 8;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FUlL_TIME:
+                        Console.WriteLine("Employee is full time");
+                        empHrs = 8;
+                        break;
 
-                case IS_PART_TIME:
-                    Console.WriteLine("Employee is part time ");
-                    empHrs = 4;
-                    break;
+                    case IS_PART_TIME:
+                        Console.WriteLine("Employee is part time ");
+                        empHrs = 4;
+                        break;
 
-                default:
-                    Console.WriteLine("Employee is absent");
-                    break;
+                    default:
+                        Console.WriteLine("Employee is absent");
+                        break;
+                }
+                empWages = empHrs * EMP_RATE_PER_HOUR;
+                totalempWage += empWages;
+                Console.WriteLine("Emp Wage : " + empWages);
             }
-            empWages = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWages);
+            Console.WriteLine("Total EmpWage:" + totalempWage);
         }
     }
 }
