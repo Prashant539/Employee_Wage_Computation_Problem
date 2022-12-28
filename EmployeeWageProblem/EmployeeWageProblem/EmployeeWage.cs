f4ecec358b6b6bf86b671bd08bb514e04bb96d17
Prashant <prashant.chaurasiya.cp@gmail.com>
2022-12-28 15:44:46 +0530

    [Prashant]EmployeeWage: Calculating EmployeeWage

--- EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
@@ -10,19 +10,24 @@ namespace EmployeeWageProblem
     {
         public void CheckEmployee()
         {
-            int Present = 1;
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWages = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
-            if (empCheck == Present)
+            if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
 
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
 
             }
+            empWages = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWages);
 
         }
 
